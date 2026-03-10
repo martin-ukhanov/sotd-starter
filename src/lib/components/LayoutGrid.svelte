81cf3e970b43c950982e199bda6458ecd8fc5430
Martin Ukhanov <martin.ukhanov@gmail.com>
2026-03-09 23:39:22 -0400

    Refactor LayoutGrid logic

--- src/lib/components/LayoutGrid.svelte
+++ src/lib/components/LayoutGrid.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
 	import { get } from 'animejs';
+	import { getBreakpoints } from '$lib/context';
+
+	const breakpoints = getBreakpoints();
 
 	let numCols = $state(0);
 	let isVisible = $state(false);
 
-	function setCols() {
-		if (isVisible) {
-			numCols = get(':root', '--layout-grid-cols', false);
-		}
-	}
-
 	function onkeydown(e: KeyboardEvent) {
 		if (e.metaKey && e.key.toLowerCase() === 'g') {
 			isVisible = !isVisible;
-			setCols();
 		}
 	}
+
+	$effect(() => {
+		if (breakpoints.lg || !breakpoints.lg) {
+			numCols = get(':root', '--layout-grid-cols', false);
+		}
+	});
 </script>
 
-<svelte:window onresize={setCols} />
 <svelte:document {onkeydown} />
 
 <div class={['pointer-events-none fixed inset-0 z-9999 layout-grid', { invisible: !isVisible }]}>
