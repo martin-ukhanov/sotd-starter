1a9f6ff0e48aa021dd3af34f0451ac7f0f40c389
Martin Ukhanov <martin.ukhanov@gmail.com>
2026-09-12 15:02:35 -0400

    Update Raf class

--- src/lib/core/raf.ts
+++ src/lib/core/raf.ts
@@ -13,10 +13,11 @@ export class Raf {
 	static #id: number | undefined;
 	static #callbacks = new CallbackList<FrameRequestCallback>();
 
-	static #run = (time: number) => {
-		this.#callbacks.run(time);
-		this.#id = requestAnimationFrame(this.#run);
-	};
+	static #nativeRequest: typeof requestAnimationFrame | undefined;
+	static #nativeCancel: typeof cancelAnimationFrame | undefined;
+
+	static #nativeId = 0;
+	static #nativeCallbacks = new Map<number, FrameRequestCallback>();
 
 	static get isRunning() {
 		return this.#id !== undefined;
@@ -24,23 +25,65 @@ export class Raf {
 
 	static add(callback: FrameRequestCallback, priority: RafPriority | number = 'default') {
 		const resolvedPriority = typeof priority === 'string' ? RAF_PRIORITIES[priority] : priority;
-		this.#callbacks.add(callback, resolvedPriority);
+		return this.#callbacks.add(callback, resolvedPriority);
 	}
 
 	static remove(callback: FrameRequestCallback) {
 		this.#callbacks.remove(callback);
 	}
 
-	static start() {
-		if (this.#id === undefined) {
-			this.#id = requestAnimationFrame(this.#run);
+	static #run: FrameRequestCallback = (time) => {
+		this.#id = this.#nativeRequest?.(this.#run);
+		this.#callbacks.run(time);
+	};
+
+	static #runNative: FrameRequestCallback = (time) => {
+		if (this.#nativeCallbacks.size === 0) return;
+
+		for (const id of [...this.#nativeCallbacks.keys()]) {
+			const callback = this.#nativeCallbacks.get(id);
+			if (!callback) continue;
+
+			this.#nativeCallbacks.delete(id);
+
+			try {
+				callback(time);
+			} catch (e) {
+				console.error('Error in requestAnimationFrame:', e);
+			}
 		}
+	};
+
+	static start() {
+		if (this.#id !== undefined) return;
+
+		this.#nativeRequest ??= window.requestAnimationFrame.bind(window);
+		this.#nativeCancel ??= window.cancelAnimationFrame.bind(window);
+
+		window.requestAnimationFrame = (callback) => {
+			const id = ++this.#nativeId;
+			this.#nativeCallbacks.set(id, callback);
+			return id;
+		};
+
+		window.cancelAnimationFrame = (id) => {
+			this.#nativeCallbacks.delete(id);
+		};
+
+		this.add(this.#runNative);
+		this.#id = this.#nativeRequest(this.#run);
 	}
 
 	static stop() {
-		if (this.#id !== undefined) {
-			cancelAnimationFrame(this.#id);
-			this.#id = undefined;
-		}
+		if (this.#id === undefined) return;
+
+		this.#nativeCancel?.(this.#id);
+		this.#id = undefined;
+
+		this.remove(this.#runNative);
+		this.#nativeCallbacks.clear();
+
+		if (this.#nativeRequest) window.requestAnimationFrame = this.#nativeRequest;
+		if (this.#nativeCancel) window.cancelAnimationFrame = this.#nativeCancel;
 	}
 }
